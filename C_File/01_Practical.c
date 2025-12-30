f154979b5e75bcd6a5c2eff8daac969701b1e692
samrjeet <010samarjeet@gmail.com>
2025-12-30 17:37:06 +0530

    Update 01_Practical.c

--- C_File/01_Practical.c
+++ C_File/01_Practical.c
@@ -1,1 +1,8 @@
+#include <stdio.h> // Standard Input Output Header
 
+int main() {
+    // Explanation: execution starts in main()
+    printf("Name: Khushbir Singh\n");
+    printf("Age: 20\n");
+    return 0;
+}
